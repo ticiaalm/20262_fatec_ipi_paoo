62e30e37f51f74833dd87af23c154220c4397350
Letícia Almeida <leticiaalmeidadev@gmail.com>
2026-08-05 16:50:56 -0300

    first commit

---
+++ intro_javascript/script.js
@@ -0,0 +1,25 @@
+/* Começando...
+Const, let, var */
+
+/* const nome = 'José'
+console.log(nome) */
+
+/* let nome = "Ana"
+console.log(nome)
+nome = "Ana Maria"
+console.log(nome) */
+
+var nome = "José"
+console.log(nome)
+nome = "João"
+console.log(nome)
+
+/* a = 2
+console.log(typeof(a))
+a = "abc"
+console.log(typeof(a))
+a.falar() */
+
+/* int a = 2;
+a = "abc";
+a.falar(); */
